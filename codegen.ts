d57db95e2cf2e757d4cd5b2c3aa6f13b99bd49d5
“Harsh <harshwalia449@gmail.com>
2024-09-11 00:40:57 +0530

    setup graphql-codegen and graphql-request

---
+++ codegen.ts
@@ -0,0 +1,20 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:8000/graphql",
+  documents: "**/*.{tsx,ts}",
+  // documents: ['app/**/*.tsx'],
+  generates: {
+    "gql/": {
+      preset: "client",
+      plugins: []
+    },
+    "./graphql.schema.json": {
+      plugins: ["introspection"]
+    }
+  }
+};
+
+export default config;
---
+++ graphql/query/user.ts
@@ -0,0 +1,6 @@
+import { graphql } from "../../gql"
+export const verifyUserGoogleTokenQuery = graphql(`#graphql
+    query verifyUserGoogleToken($token:String!){
+        verifyGoogleToken(token: $token)
+    }
+`)
