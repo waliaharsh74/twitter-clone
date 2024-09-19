2653692a8844fa1cf3b62fbe0c6475c108ac2673
“Harsh <harshwalia449@gmail.com>
2024-09-20 00:13:05 +0530

    added tweet text

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useCallback } from "react";
 import Image from "next/image";
 import { BsTwitter } from "react-icons/bs";
-import { BiBell, BiBookmark, BiEnvelope, BiHash, BiHome, BiUser } from "react-icons/bi";
+import { BiBell, BiBookmark, BiEnvelope, BiHash, BiHome, BiImageAlt, BiUser } from "react-icons/bi";
 import { CredentialResponse, GoogleLogin } from '@react-oauth/google'
 import FeedCard from "./components/FeedCard/page";
 import toast from "react-hot-toast";
@@ -10,6 +10,7 @@ import { graphqlClient } from "../clients/api";
 import { verifyUserGoogleTokenQuery } from "../graphql/query/user"
 import { useCurrentUser } from "@/hooks/user";
 import { useQueryClient } from "@tanstack/react-query";
+import TweetCard from "./components/TweetCard/page";
 
 
 interface TwitterSidebarButtons {
@@ -58,10 +59,16 @@ export default function Home() {
     console.log(verifyGoogleToken);
 
     if (verifyGoogleToken) window.localStorage.setItem('__twitter_token', verifyGoogleToken)
-    await queryClient.invalidateQueries(["current-user"])
+    await queryClient.invalidateQueries({ queryKey: ["current-user"] })
 
 
   }, [queryClient])
+  const handleSelectImage = useCallback(() => {
+    const input = document.createElement("input")
+    input.setAttribute("type", "file")
+    input.setAttribute("accept", "image/*")
+    input.click();
+  }, [])
 
   return (
     <div className="">
@@ -88,6 +95,39 @@ export default function Home() {
         </div>
 
         <div className="col-span-6 border-r-[0.5px] border-l-[0.5px] border-gray-600 h-screen overflow-scroll scrollbar-hidden">
+
+          <div className='border border-l-0 border-r-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer'>
+            <div className="grid grid-cols-12 gap-2">
+              <div className='col-span-1'>
+                {user?.profileImageURL && <Image src={user?.profileImageURL} alt='User Avatar' className="rounded-full" width={50} height={50} />}
+
+              </div>
+              <div className='col-span-10'>
+
+                <textarea className="border-b h-10 w-full bg-transparent text-xl p-3" placeholder="What's happening?" rows={6} id=""></textarea>
+                <div className="mt-2 flex justify-between items-center ">
+
+                  <BiImageAlt onClick={handleSelectImage} className="text-2xl" />
+                  <button className="bg-[#1d9bf0] text-sm px-4 py-2 mt-4 rounded-full  ">Tweet</button>
+                </div>
+                {/* <div className='flex justify-between mt-5 text-xl items-start pr-10 w-[80%]'>
+                  <div>
+                    <BiMessageRounded />
+                  </div>
+                  <div>
+                    <FaRetweet />
+                  </div>
+                  <div>
+                    <AiOutlineHeart />
+                  </div>
+                  <div>
+                    <BiUpload />
+                  </div>
+                </div> */}
+              </div>
+            </div>
+          </div>
+
           <FeedCard />
           <FeedCard />
           <FeedCard />
